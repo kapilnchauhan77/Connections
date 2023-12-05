8fc8764c84fb50e64cdf5f39a2940312f9afaa99
kapilnchauhan77 <kapilnchauhan77@gmail.com>
2023-12-05 12:56:22 -0500

    Fixed contact

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -1,13 +1,6 @@
-import { useState } from 'react'
-import { Switch } from '@headlessui/react'
 import { useNavigate } from 'react-router-dom'
 
-function classNames(...classes: any[]) {
-  return classes.filter(Boolean).join(' ')
-}
-
 export default function Contact() {
-  const [agreed, setAgreed] = useState(false)
   const nav = useNavigate()
 
   return (
@@ -24,7 +17,7 @@ export default function Contact() {
 
         </div>
         <div>
-        <form method="POST" className="mx-auto mt-10 max-w-xl sm:mt-15">
+        <form className="space-y-6" onSubmit={()=>(nav('/'))}>
           <div className="grid grid-cols-1 gap-x-8 gap-y-6 sm:grid-cols-2">
             <div>
               <label htmlFor="first-name" className="block text-sm font-semibold leading-6 text-gray-900">
@@ -36,6 +29,7 @@ export default function Contact() {
                   name="first-name"
                   id="first-name"
                   autoComplete="given-name"
+                  required
                   className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                 />
               </div>
@@ -51,6 +45,7 @@ export default function Contact() {
                   id="last-name"
                   autoComplete="family-name"
                   className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                  required
                 />
               </div>
             </div>
@@ -63,6 +58,7 @@ export default function Contact() {
                   type="email"
                   name="email"
                   id="email"
+                  required
                   autoComplete="email"
                   className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                 />
@@ -79,42 +75,14 @@ export default function Contact() {
                   rows={4}
                   className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                   defaultValue={''}
+                  required
                 />
               </div>
             </div>
-            <Switch.Group as="div" className="flex gap-x-4 sm:col-span-2">
-              <div className="flex h-6 items-center">
-                <Switch
-                  checked={agreed}
-                  onChange={setAgreed}
-                  className={classNames(
-                    agreed ? 'bg-indigo-600' : 'bg-gray-200',
-                    'flex w-8 flex-none cursor-pointer rounded-full p-px ring-1 ring-inset ring-gray-900/5 transition-colors duration-200 ease-in-out focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600'
-                  )}
-                >
-                  <span className="sr-only">Agree to policies</span>
-                  <span
-                    aria-hidden="true"
-                    className={classNames(
-                      agreed ? 'translate-x-3.5' : 'translate-x-0',
-                      'h-4 w-4 transform rounded-full bg-white shadow-sm ring-1 ring-gray-900/5 transition duration-200 ease-in-out'
-                    )}
-                  />
-                </Switch>
-              </div>
-              <Switch.Label className="text-sm leading-6 text-gray-600">
-                By selecting this, you agree to our{' '}
-                <a href="#" className="font-semibold text-indigo-600">
-                  privacy&nbsp;policy
-                </a>
-                .
-              </Switch.Label>
-            </Switch.Group>
           </div>
           <div className="mt-10">
             <button
               type="submit"
-              onSubmit={()=>{nav("/")}}
               className="block w-full rounded-md bg-indigo-600 px-3.5 py-2.5 text-center text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
             >
               Let's talk
